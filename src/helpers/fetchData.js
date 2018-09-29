b503a0bb2d5b4d750e03943999134ca89fed775e
monisahmad <monis.ahmad42@gmail.com>
2018-09-29 14:04:40 +0530

    fix: Refactor URL

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -2,23 +2,23 @@ import databaseRef from '../config/firebase';
 import { githubApi } from '../config/apiRoutes';
 
 const fetchData = async (username) => {
-  const response = await fetch(`${githubApi}${username}`);
-  const data = await response.json();
-  if (data.message === 'Not Found') {
-    return false;
-  }
-  const formattedDate = new Date(data.created_at).toLocaleDateString('en-US');
-  const dataSource = {
-    key: data.node_id,
-    username: data.login,
-    name: data.name,
-    public_repos: data.public_repos,
-    public_gists: data.public_gists,
-    followers: data.followers,
-    following: data.following,
-    created_at: formattedDate,
-  };
   try {
+    const response = await fetch(`${githubApi.userData}${username}`);
+    const data = await response.json();
+    if (data.message === 'Not Found') {
+      return false;
+    }
+    const formattedDate = new Date(data.created_at).toLocaleDateString('en-US');
+    const dataSource = {
+      key: data.node_id,
+      username: data.login,
+      name: data.name,
+      public_repos: data.public_repos,
+      public_gists: data.public_gists,
+      followers: data.followers,
+      following: data.following,
+      created_at: formattedDate,
+    };
     await databaseRef.push(dataSource);
     return true;
   } catch (error) {
